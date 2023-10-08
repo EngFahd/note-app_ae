6b6d36c72594b6ea56b213c9d5d9db2a7142ca47
EngFahd <1616fahdmahmoud@gmail.com>
2023-10-08 17:07:47 +0300

    add blocObserver

--- lib/widgites/addNoteButton.dart
+++ lib/widgites/addNoteButton.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:note_app/constant.dart';
 import 'package:note_app/cubite/cubit/note_cubit_cubit.dart';
 import 'package:note_app/widgites/coustemTextFiled.dart';
@@ -21,10 +22,13 @@ class AddNoteButton extends StatelessWidget {
           }
         },
         builder: (context, state) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SingleChildScrollView(
-              child: AddNoteForm(),
+          return ModalProgressHUD(
+            inAsyncCall: state is NoteCubitLoading ? true : false,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: AddNoteForm(),
+              ),
             ),
           );
         },
