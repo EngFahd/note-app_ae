012e362edf5eaa900d3492a74b9862365f6dd11f
EngFahd <1616fahdmahmoud@gmail.com>
2023-10-12 09:34:44 +0300

    create list colors

--- lib/widgites/AddnoteSheet.dart
+++ lib/widgites/AddnoteSheet.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
@@ -51,6 +50,10 @@ class _AddNoteFormState extends State<AddNoteForm> {
             const SizedBox(
               height: 20,
             ),
+            ColorListViwe(),
+            const SizedBox(
+              height: 20,
+            ),
             BlocBuilder<NoteCubitCubit, NoteCubitState>(
               builder: (context, state) {
                 return customButton(
@@ -59,7 +62,8 @@ class _AddNoteFormState extends State<AddNoteForm> {
                     if (glopalKey.currentState!.validate()) {
                       glopalKey.currentState!.save();
                       // formateDate(buttern)
-                      var formateDate = DateFormat('dd-MM-yyyy').format(DateTime.now());
+                      var formateDate =
+                          DateFormat('dd-MM-yyyy').format(DateTime.now());
                       NoteModel noteModel = NoteModel(
                           titel: titel!,
                           subTitel: subTitel!,
@@ -77,9 +81,74 @@ class _AddNoteFormState extends State<AddNoteForm> {
             ),
             const SizedBox(
               height: 20,
-            )
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key, required this.Isactive, required this.color});
+  final bool Isactive;
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Isactive
+        ? CircleAvatar(
+            radius: 36,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              backgroundColor: color,
+              radius: 33,
+            ),
+          )
+        : CircleAvatar(
+            backgroundColor: color,
+            radius: 38,
+          );
+  }
+}
+
+class ColorListViwe extends StatefulWidget {
+  const ColorListViwe({super.key});
+
+  @override
+  State<ColorListViwe> createState() => _ColorListViweState();
+}
+
+class _ColorListViweState extends State<ColorListViwe> {
+  List<Color> colors = [
+    Color(0xffB80C09),
+    Color(0xff141301),
+    Color(0xffB7B5B3),
+    Color(0xff545E56),
+    Color(0xff6B2B06),
+  ];
+  int currentState = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 38 * 2,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: colors.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              currentState = index;
+              setState(() {});
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: ColorItem(
+                color: colors[index],
+                Isactive: currentState == index,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
